0282c724d72e3d4ee4622a59c8a078ac687b9c39
niermoon <emyuto@icloud.com>
2022-12-30 04:57:06 -0500

    bettie

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        //Calc
+        static void Main(string[] args)
+        {
+            Console.Write("Welcome to Jay's Calculator!");
+
+            System.Threading.Thread.Sleep(1000);
+                Console.Clear();
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            System.Threading.Thread.Sleep(1000);
+                Console.Clear();
+
+            Console.WriteLine(num1 + num2);
+            System.Threading.Thread.Sleep(500);
+
+            Console.WriteLine("Thank You for using Jay's Calc :)");
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
